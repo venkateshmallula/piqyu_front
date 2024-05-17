9e9e7f04892110f0cd06fc5d71cc74635448f123
venkateshmallula <mallula.venky766@gmail.com>
2024-05-17 16:58:02 +0530

    ll

--- src/pages/navBar.js
+++ src/pages/navBar.js
@@ -45,7 +45,7 @@ const Navbar = () => {
 
   return (
     <Flex as="nav" alignItems="center" height="60px" backgroundColor="#dfdfe6">
-      <Heading as="h1" paddingLeft="10" color="blueviolet">
+      <Heading as="h3" paddingLeft="10" color="blueviolet">
         PIQYU SMARTFLOW
       </Heading>
       <Spacer />
